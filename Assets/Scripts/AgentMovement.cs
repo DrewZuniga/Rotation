6a39d312023d179f395366632005ce0fb72d34a4
Drew Zuniga <drewz137@yahoo.com>
2020-10-29 12:05:40 -0400

    First Commit

---
+++ Assets/Scripts/AgentMovement.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMovement : MonoBehaviour
+{
+    protected CharacterController characterController;
+    protected Animation agentAnimations;
+    public float movementSpeed;
+    public float gravity;
+    public float rotationSpeed;
+
+    public int angleRotationThreshold;
+
+    protected Vector3 moveDirection = Vector3.zero;
+
+    protected float desiredRotationAngle = 0;
+
+    int inputVerticalDirection = 0;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        agentAnimations = GetComponent<Animation>();
+    }
+
+    public void HandleMovement(Vector2 input)
+    {
+        if (characterController.isGrounded)
+        {
+            if (input.y != 0)
+            {
+                if (input.y > 0)
+                {
+                    inputVerticalDirection = Mathf.CeilToInt(input.y);
+                }
+                else
+                {
+                    inputVerticalDirection = Mathf.FloorToInt(input.y);
+                }
+                moveDirection = input.y * transform.forward * movementSpeed;
+            }
+            else
+            {
+                //agentAnimations.SetMovementFloat(0);
+                moveDirection = Vector3.zero;
+            }
+        }
+
+
+    }
+
+    public void HandleMovementDirection(Vector3 input)
+    {
+        desiredRotationAngle = Vector3.Angle(transform.forward, input);
+        var crossProduct = Vector3.Cross(transform.forward, input).y;
+        if (crossProduct < 0)
+        {
+            desiredRotationAngle *= -1;
+        }
+    }
+
+    private void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            if (moveDirection.magnitude > 0)
+            {
+                //var animationSpeedMultiplier = agentAnimations.SetCorrectAnimation(desiredRotationAngle, angleRotationThreshold, inputVerticalDirection);
+                RotateAgent();
+                //moveDirection *= animationSpeedMultiplier;
+            }
+        }
+        moveDirection.y -= gravity;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void RotateAgent()
+    {
+        if (desiredRotationAngle > angleRotationThreshold || desiredRotationAngle < -angleRotationThreshold)
+        {
+            transform.Rotate(Vector3.up * desiredRotationAngle * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    //sending value to anyone listening
+    public Vector2 OnMovementInput { get; set; }
+    //sending direction to target
+    public Vector3 OnMovementDirectionInput { get; set; }
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        //locking cursor to center screen
+        Cursor.lockState = CursorLockMode.Locked;
+        mainCamera = Camera.main;
+
+    }
+
+    private void Update()
+    {
+        GetMovementInput();
+        GetMovementDirection();
+    }
+
+    private void GetMovementDirection()
+    {
+        OnMovementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //if anything is listening to delagate, send input
+       // Debug.Log(OnMovementInput);
+    }
+
+    private void GetMovementInput()
+    {
+
+        var cameraForwardDir = mainCamera.transform.forward;
+        //Debug.DrawRay(mainCamera.transform.position, cameraForwardDir * 10, Color.red);
+
+        OnMovementDirectionInput = Vector3.Scale(cameraForwardDir, (Vector3.right + Vector3.forward));
+      //  Debug.DrawRay(mainCamera.transform.position, directionMoveIn * 10, Color.blue);
+       
+    }
+}
